edba5a507e54ca05b8f4e5f522b5689c0752d0f2
catethysis <catethysis@gmail.com>
2018-01-09 18:05:31 +0300

    Get build.json filename from command line

--- config_parser.c
+++ config_parser.c
@@ -4,17 +4,24 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "cJSON/cJSON.h"
-#include "timer.h"
+#include "timer/timer.h"
+#include <unistd.h>
+#include <dirent.h>
+
+const uint32_t name_max_length = 100;
+const uint32_t path_max_length = 300;
+const uint32_t flags_max_length = 100;
+const uint32_t sources_max_count = 200;
 
 struct {
-	char name[100];
+	char name[name_max_length];
 	struct {
-		char path[200];
-		char name[100];
-		char flags[100];
-	} sources[200];
+		char path[path_max_length];
+		char name[name_max_length];
+		char flags[flags_max_length];
+	} sources[sources_max_count];
 	uint16_t n_sources;
-	char paths[100][100];
+	char paths[sources_max_count][path_max_length];
 	uint16_t n_paths;
 } project;
 
@@ -27,7 +34,7 @@ void recursive_parser(uint8_t depth, cJSON *json_sources, char *flags_string) {
 	}
 
 	const cJSON *path = cJSON_GetObjectItemCaseSensitive(json_sources, "path");
-	char path_string[200] = ".";
+	char path_string[path_max_length] = ".";
 	if(cJSON_IsString(path))
 		strcpy(path_string, path->valuestring);
 	strcat(path_string, "/");
@@ -59,13 +66,13 @@ void parse_config(char *json) {
 		printf("Error while JSON parsing.'n");
 		return;
 	}
-	timer_tick("Parse");
+	timer_tick("JSON parsing");
 
 	const cJSON *name = cJSON_GetObjectItemCaseSensitive(config, "project");
 	if (cJSON_IsString(name) && (name->valuestring != NULL))
 		strcpy(project.name, name->valuestring);
 
-	char flags_string[100] = "";
+	char flags_string[flags_max_length] = "";
 	const cJSON *sources = cJSON_GetObjectItemCaseSensitive(config, "sources");
 	cJSON *source;
 	cJSON_ArrayForEach(source, sources) {
@@ -91,16 +98,7 @@ size_t read_config(char *path, char **config) {
 	return config_len;
 }
 
-int main() {
-	timer_start();
-	
-	char *config;
-	size_t config_len = read_config("build.json", &config);
-	timer_tick("Read");
-	project.n_sources = 0;
-	parse_config(config);
-	timer_tick("File list");
-
+void list_project_files() {
 	printf("Project \"%s\"\n", project.name);
 	printf("\nSources:\n");
 	for(uint16_t i = 0; i < project.n_sources; i++)
@@ -108,6 +106,32 @@ int main() {
 	printf("\nPaths:\n");
 	for(uint16_t i = 0; i < project.n_paths; i++)
 		printf("%s\n", project.paths[i]);
+}
+
+int main(int argc, char *argv[]) {
+	timer_start();
+
+	// Find build.json in command line arguments and check if it exists
+	char *config_file;
+	if(argc == 2)
+		config_file = argv[1];
+	else
+		config_file = "./build.json";
+
+	if (access(config_file, F_OK) == -1) {
+		printf("Build config file %s not found, terminating.\n", config_file);
+		return -1;
+	}
+
+	char *config;
+	read_config(config_file, &config);
+	timer_tick("Read");
+
+	project.n_sources = 0;
+	parse_config(config);
+	timer_tick("Config parsing");
+
+	list_project_files();
 	timer_end("Total");
 
 	return 0;
